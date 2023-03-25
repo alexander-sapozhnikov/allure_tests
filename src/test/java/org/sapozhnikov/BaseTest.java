07828f5947dacde7d446d6bf77334b99168ec1da
alexandr <cawa.sapog@yandex.ru>
2023-03-25 21:23:40 +0400

    add parametrize tests

--- src/test/java/org/sapozhnikov/BaseTest.java
+++ src/test/java/org/sapozhnikov/BaseTest.java
@@ -3,7 +3,7 @@ package org.sapozhnikov;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
@@ -43,7 +43,7 @@ public class BaseTest implements IDriverControl {
         mainPage = new MainPage(driver);
     }
 
-    @AfterClass
+    @AfterMethod
     public void close() {
         driver.quit();
     }
--- src/test/java/org/sapozhnikov/CheckShoppingCart.java
+++ src/test/java/org/sapozhnikov/CheckShoppingCart.java
@@ -3,16 +3,25 @@ package org.sapozhnikov;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 @Listeners({FailedTestListener.class})
 public class CheckShoppingCart extends BaseTest {
-    @Test
+    @DataProvider(name = "check_cart_data")
+    public Object[][] checkCartData() {
+        return new Object[][] {
+                {"Кулон и серьги \"Звезда\""},
+                { "Кулон и серьги \"Бирюза\"" },
+                { "Кулон «Дорога. Утро»"},
+        };
+    }
+
+    @Test(dataProvider = "check_cart_data")
     @Description("Check shopping cart")
     @Severity(SeverityLevel.MINOR)
-    public void checkCart() {
-        String nameImage = "Кулон и серьги \"Звезда\"";
+    public void checkCart(String nameImage) {
         CatalogPage catalogPage = mainPage.openCatalog();
         CategoryPage jewelryArtPage = catalogPage.open("Ювелирное искусство");
         int prevCost = jewelryArtPage.getCost(nameImage);
